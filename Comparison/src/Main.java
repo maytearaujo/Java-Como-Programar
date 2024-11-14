6572ed16a7492114de1bc1ac690bcd95bad29ff0
Maytê <maytearaujo@yahoo.com.br>
2024-11-14 11:45:32 -0300

    Comparassion

---
+++ Comparison/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int number1 = 0,
+                number2 = 0;
+
+        System.out.print("Enter first integer: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        number2 = input.nextInt();
+
+        if (number1 == number2)
+            System.out.printf("%d == %d%n", number1, number2);
+
+        if (number1 != number2)
+            System.out.printf("%d != %d%n", number1, number2);
+
+        if (number1 < number2)
+            System.out.printf("%d < %d%n", number1, number2);
+
+        if (number1 > number2)
+            System.out.printf("%d > %d%n", number1, number2);
+
+        if (number1 <= number2)
+            System.out.printf("%d <= %d%n", number1, number2);
+
+        if (number1 >= number2)
+            System.out.printf("%d >= %d%n", number1, number2);
+    }
+}
---
+++ Precedencias/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2,
+                b = 3,
+                c = 7,
+                x = 5,
+                y = 0;
+
+        y = a * x * x + b * x + c;
+
+        System.out.printf("Resultado %d%n", y);
+    }
+}
