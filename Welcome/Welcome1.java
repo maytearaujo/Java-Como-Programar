37d993918ba416f67e9406620b571f3631a88847
Maytê <maytearaujo@yahoo.com.br>
2024-11-14 08:02:02 -0300

    First Commit

---
+++ Welcome/Welcome1.java
@@ -0,0 +1,10 @@
+public class Welcome1
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+		
+		System.out.println("Welcome");
+	}
+}
